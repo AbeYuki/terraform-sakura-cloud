d907e65d1aaaa1124ad101c95e924dc6eaa15f58
AbeYuki <aim4highergg@gmail.com>
2023-02-19 20:44:01 +0900

    Modify backend s3

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,18 @@
 terraform {
+  backend "s3" {
+    bucket = "aimhighergg-tfstate"
+    region = "ap-northeast-1"
+    key = "sakura-cloud.tfstate"
+    encrypt = true
+  }
   required_providers {
     sakuracloud = {
       source  = "sacloud/sakuracloud"
       version = "2.21.0"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
     }
   }
 }
@@ -10,4 +20,8 @@ provider "sakuracloud" {
   token  = var.SAKURACLOUD_ACCESS_TOKEN
   secret = var.SAKURACLOUD_ACCESS_TOKEN_SECRET
   zone   = var.SAKURACLOUD_ZONE
+}
+provider "aws" {
+  profile = "default"
+  region  = "ap-northeast-1"
 }
